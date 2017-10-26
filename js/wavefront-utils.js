544343a9f8a8e4ddd25c3818bc7b3f083e0eb3c1
Saint Pierre Thomas <s1pierro@localhost.localdomain>
2017-10-26 15:15:39 +0200

    wip updates

--- js/wavefront-utils.js
+++ js/wavefront-utils.js
@@ -283,10 +283,10 @@ function MovePiece (p, x, y)
 		}
 		switchMaterial ( target, "dead");
 
-	$("body").append('<object hidden type="audio/mpeg" width="100" height="40" data="chesssound/start1.ogg"><param name="filename" value="chesssound/capture2.ogg" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>');
+	$("body").append('<object hidden type="audio/mpeg" width="100" height="40" data="chesssound/capture2.ogg"><param name="filename" value="chesssound/capture2.ogg" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>');
 console.log("capture");
 	}
-	else $("body").append('<object hidden type="audio/mpeg" width="100" height="40" data="chesssound/start1.ogg"><param name="filename" value="chesssound/mov2.ogg" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>');
+	else $("body").append('<object hidden type="audio/mpeg" width="100" height="40" data="chesssound/mov2.ogg"><param name="filename" value="chesssound/mov2.ogg" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>');
 
 
 	plateau[newX][newY] = p;
