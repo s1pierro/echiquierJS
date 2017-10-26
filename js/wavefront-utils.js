f1874cdfe31768a56be0b6b7946d9852bd9119d1
Saint Pierre Thomas <s1pierro@localhost.localdomain>
2017-10-26 15:11:39 +0200

    wip updates

--- js/wavefront-utils.js
+++ js/wavefront-utils.js
@@ -283,8 +283,12 @@ function MovePiece (p, x, y)
 		}
 		switchMaterial ( target, "dead");
 
-	
+	$("body").append('<object hidden type="audio/mpeg" width="100" height="40" data="chesssound/start1.ogg"><param name="filename" value="chesssound/capture2.ogg" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>');
+
 	}
+	else $("body").append('<object hidden type="audio/mpeg" width="100" height="40" data="chesssound/start1.ogg"><param name="filename" value="chesssound/mov2.ogg" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>');
+
+
 	plateau[newX][newY] = p;
 		console.log(plateau.join('\n') + '\n\n');
 	//buffer = $.extend(true, {}, wvft);
