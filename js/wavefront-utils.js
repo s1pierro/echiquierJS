dad007763796bd01ff38596d4519c83978374e26
Saint Pierre Thomas <s1pierro@localhost.localdomain>
2017-10-28 01:53:41 +0200

    wip updates

--- js/wavefront-utils.js
+++ js/wavefront-utils.js
@@ -287,7 +287,7 @@ function MovePiece (p, x, y)
 	$("body").append('<object id="capture" hidden type="audio/mpeg" width="100" height="40" data="chesssound/capture2.ogg"><param name="filename" value="chesssound/capture2.ogg" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>');
 console.log("capture");
 	}
-	$("body").append('<object hidden type="audio/mpeg" width="100" height="40" data="chesssound/mov2.ogg"><param name="filename" value="chesssound/mov2.ogg" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>');
+	$("body").append('<object hidden type="audio/mpeg" width="100" height="40" data="chesssound/move1.ogg"><param name="filename" value="chesssound/move1.ogg" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>');
 
 
 	plateau[newX][newY] = p;
